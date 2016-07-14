9651a44a3ba466b98e548d0b2d9d5d8b44f4fc21
Ryo <r.otaka@outlook.jp>
2016-07-14 20:53:40 +0900

    KenFujita合併

--- app/src/main/java/com/t_robop/smaon/MainActivity.java
+++ app/src/main/java/com/t_robop/smaon/MainActivity.java
@@ -1,5 +1,6 @@
 package com.t_robop.smaon;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -20,12 +21,34 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     static TextView txt3;
-    static String ondo;
+    static TextView txt;
+    static TextView txt2;
+    static TextView txt5;                                   //てーあん
+    static TextView txt6;
+    static String ondo;                                     //ホリエモンの温度
+    static String ondocp;
+    static String tenki;                                    //その日の天気
+    static String Str;                                      //ラズパイパイの日付
+    static String Str2;                                     //ラズパイパイの温度
+    static String Str2cp;
+    static double Txt=0.0;                                       //ホリエモンの温度を数値化
+    static double Txt2=0.0;                                      //ラズパイパイの温度を数値化
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        txt3 = (TextView)findViewById(R.id.textView3);      //温度（現在は名前）
+        txt3 = (TextView)findViewById(R.id.textView3);      //温度（ラズパイ）
+        txt = (TextView)findViewById(R.id.textView);        //日付
+        txt2 = (TextView)findViewById(R.id.textView2);      //温度（ホリエモン）
+        txt5 = (TextView)findViewById(R.id.textView5);      //提案文
+        txt6 = (TextView)findViewById(R.id.textView6);
+
+        Intent intent = getIntent();
+        Str = intent.getStringExtra("date");
+        Str2 = intent.getStringExtra("cels");
+
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
@@ -34,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static class PlaceholderFragment extends Fragment {
-        private final String uri = "http://weather.livedoor.com/forecast/webservice/json/v1?city=400040";       //JSONデータのあるURLを設定
+        private final String uri = "http://api.openweathermap.org/data/2.5/forecast/city?id=1859171&APPID=d943f13cb21447ca156076c493e2f236";             //JSONデータのあるURLを設定
 
         public PlaceholderFragment() {
         }
@@ -53,11 +76,11 @@ public class MainActivity extends AppCompatActivity {
                         return;
                     }
                     try{
-                        JSONArray eventArray = result.getJSONArray("forecasts");    //配列データを取得
+                        JSONArray eventArray = result.getJSONArray("list");    //配列データを取得
                         for (int i = 0; i < 1; i++) {                     //JSONのデータを追加
                             JSONObject eventObj = eventArray.getJSONObject(1);
-                            JSONObject event = eventObj.getJSONObject("temperature").getJSONObject("min");
-                            ondo = event.getString("celsius");
+                            JSONObject event = eventObj.getJSONObject("main");
+                            ondo = event.getString("temp");
                             txt3.setText(ondo);
                         }
 
@@ -66,6 +89,27 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                         showLoadError(); // エラーメッセージを表示
                     }
+                    txt6.setText(Str2);
+                    ondocp = ondo.substring(0,ondo.length());
+                    Str2cp = Str2.substring(0,Str2.length());
+                    Txt = Math.floor(Double.parseDouble(ondocp)-273);
+                    Txt2 = Double.parseDouble(Str2cp);
+                    txt3.setText(String.valueOf(Txt));
+                    if(Txt < Txt2){             //堀江<パイ
+                         txt5.setText("現在予想より温度が高くなっております。\n");
+                        if(Txt2-Txt > 5){
+                            txt5.append("水分補給をこまめに行いましょう。\n");
+                                                 }
+                                      }
+                                     else if(Txt2 < Txt){
+                                               txt5.setText("現在予想より温度が低くなっております。\n");
+                        if(Txt-Txt2 > 5){
+                            txt5.append("上着を羽織って暖かくするようにしましょう。\n");
+                        }
+                    }
+                    txt5.append("体調管理に気を付けましょう。\n");
+
+
                 }
                 // 実行中
                 public void progressUpdate(int progress) {
