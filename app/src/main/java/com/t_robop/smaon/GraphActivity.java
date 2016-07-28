85205fa7300e1eea392a3826e4940f3a3eaaa17b
black ehu <fehuehuf@gmail.com>
2016-07-28 19:37:01 +0900

    testtes

--- app/src/main/java/com/t_robop/smaon/GraphActivity.java
+++ app/src/main/java/com/t_robop/smaon/GraphActivity.java
@@ -18,7 +18,7 @@ import com.github.mikephil.charting.data.Entry;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-
+//
 public class GraphActivity extends AppCompatActivity {
     LineChart lineChart;
 
